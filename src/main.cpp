705c9e78c0170b01f8fe30917d0157f28af3a517
HeZhijun4030 <115980984+HeZhijun4030@users.noreply.github.com>
2026-07-24 22:25:43 +0800

    LICENSE

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
-﻿/**
+﻿/*
+ * Copyright (c) 2026 HeZhijun (CodeManStudio)
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/**
  * @file main.cpp
  * @brief MK-ServerLauncher 桌面版主程序入口
  * @author CodeManStudio
